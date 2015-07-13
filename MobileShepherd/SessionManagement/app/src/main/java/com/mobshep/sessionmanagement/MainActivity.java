3758d46771796fc5ec784e34f79c22cdc5bfd8bc
SeanDuggan <sean.duggan4@gmail.com>
2015-07-13 22:05:24 +0100

    Finishing of Session Management Lesson App.
    A user will get the key to complete the level with the presence of an active (not null) session within the sharedpreferences on the device.
    
    Need to add network call to grab key from Shepherd instance.

--- MobileShepherd/SessionManagement/app/src/main/java/com/mobshep/sessionmanagement/MainActivity.java
+++ MobileShepherd/SessionManagement/app/src/main/java/com/mobshep/sessionmanagement/MainActivity.java
@@ -1,19 +1,57 @@
 package com.mobshep.sessionmanagement;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    Button getKey;
+    EditText showKey;
+    SharedPreferences storedPref;
+    SharedPreferences.Editor toEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        storedPref = getSharedPreferences("Sessions", MODE_WORLD_READABLE);
+        toEdit = storedPref.edit();
+        toEdit.putString("SessionToken", "null");
+        toEdit.commit();
+
     }
+
+
+    public void getKeyClicked(View v) {
+
+        SharedPreferences prefs = this.getSharedPreferences("SessionToken", MODE_PRIVATE);
+        String session = prefs.getString("SessionToken", null);
+
+        if (session == "null") {
+            Toast noSession = Toast.makeText(MainActivity.this,
+                    "You do not have an active session!", Toast.LENGTH_LONG);
+            noSession.show();
+        }
+
+            else{
+            Toast gettingKey = Toast.makeText(MainActivity.this,
+                    "Getting the key...", Toast.LENGTH_LONG);
+            gettingKey.show();
+            }
+
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,4 +87,5 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
 
     }
+
 }
