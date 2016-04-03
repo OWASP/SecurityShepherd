51a190b2cdf4eee22a2317e74a377695da40880f
Sean <Sean@DESKTOP-1P4F02Q>
2016-04-03 21:15:01 +0000

    Various changes made to TLS based levels:
    1. Removing old deprecated code of later challenges (other code needs to
    be updated) due to the fact that apache.http is now deprecated.
    
    2. Restarted ITLS1, keeping deprecated code this time due to the fact
    it's all in plaintext and will be sufficient for an easy introduction.

--- MobileShepherd/InsufficientTLS/app/proguard-rules.pro
+++ MobileShepherd/InsufficientTLS/app/proguard-rules.pro
@@ -1,6 +1,6 @@
 # Add project specific ProGuard rules here.
 # By default, the flags in this file are appended to flags specified
-# in C:\Users\IBM_ADMIN\AppData\Local\Android\sdk/tools/proguard/proguard-android.txt
+# in C:\Users\Sean\AppData\Local\Android\sdk/tools/proguard/proguard-android.txt
 # You can edit the include path and order by changing the proguardFiles
 # directive in build.gradle.
 #
--- MobileShepherd/InsufficientTLS/app/src/main/java/com/mobshep/insufficienttls/Preferences.java
+++ MobileShepherd/InsufficientTLS/app/src/main/java/com/mobshep/insufficienttls/Preferences.java
@@ -1,29 +1,53 @@
 package com.mobshep.insufficienttls;
 
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
- 
-public class Preferences extends PreferenceActivity {
- 
- @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content,
-    new PrefsFragment()).commit();
-  PreferenceManager.setDefaultValues(Preferences.this, R.layout.preferences, false);
-   
-    }
-  
- public static class PrefsFragment extends PreferenceFragment {
-   
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    
-   super.onCreate(savedInstanceState);
-   addPreferencesFromResource(R.layout.preferences);
-  }
- }
-  
+
+/**
+ * This file is part of the Security Shepherd Project.
+ * 
+ * The Security Shepherd project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.<br/>
+ * 
+ * The Security Shepherd project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.<br/>
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with the Security Shepherd project.  If not, see <http://www.gnu.org/licenses/>. 
+ * 
+ * @author Sean Duggan
+ */
+
+public class Preferences extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Display the fragment as the main content.
+		FragmentManager mFragmentManager = getFragmentManager();
+		FragmentTransaction mFragmentTransaction = mFragmentManager
+				.beginTransaction();
+		PrefsFragment mPrefsFragment = new PrefsFragment();
+		mFragmentTransaction.replace(android.R.id.content, mPrefsFragment);
+		mFragmentTransaction.commit();
+	}
+
+	public static class PrefsFragment extends PreferenceFragment {
+
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+
+			// Load the preferences from an XML resource
+			addPreferencesFromResource(R.layout.preferences);
+		}
+	}
 }
