97a81984de4a2970b57eeed44db1bc701081df51
mdenihan@axway.com <markdenihan@gmail.com>
2018-08-15 14:42:50 +0100

    #343 Making the Setup Page Pretty

--- src/main/java/servlets/Setup.java
+++ src/main/java/servlets/Setup.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
@@ -9,13 +10,14 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
@@ -23,60 +25,83 @@ import org.apache.log4j.Logger;
 import dbProcs.Constants;
 import dbProcs.Database;
 import utils.InstallationException;
+import utils.Validate;
 
 public class Setup extends HttpServlet {
 	private static org.apache.log4j.Logger log = Logger.getLogger(Setup.class);
 	private static final long serialVersionUID = -892181347446991016L;
 
 	public void doPost (HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
-		HttpSession ses = request.getSession(true);
+		//Translation Stuff
+		Locale locale = new Locale(Validate.validateLanguage(request.getSession()));
+		ResourceBundle errors = ResourceBundle.getBundle("i18n.servlets.errors", locale);
+		ResourceBundle bundle = ResourceBundle.getBundle("i18n.text", locale);
+		
+		//Output Stuff
+		PrintWriter out = response.getWriter();
+		String htmlOutput = new String();
+		boolean success = false;
+		try 
+		{
+			//Parameters From Form
+			String dbHost = request.getParameter("dbhost");
+			String dbPort = request.getParameter("dbport");
+			String dbUser = request.getParameter("dbuser");
+			String dbPass = request.getParameter("dbpass");
+			String dbAuth = request.getParameter("dbauth");
+			String dbOverride = request.getParameter("dboverride");
 
-		String dbHost = request.getParameter("dbhost");
-		String dbPort = request.getParameter("dbport");
-		String dbUser = request.getParameter("dbuser");
-		String dbPass = request.getParameter("dbpass");
-		String dbAuth = request.getParameter("dbauth");
-		String dbOverride = request.getParameter("dboverride");
+			String auth = new String(Files.readAllBytes(Paths.get(Constants.SETUP_AUTH)));
 
-		String auth = new String(Files.readAllBytes(Paths.get(Constants.SETUP_AUTH)));
+			StringBuffer dbProp = new StringBuffer();
+			dbProp.append("databaseConnectionURL=jdbc:mysql://" + dbHost + ":" + dbPort + "/");
+			dbProp.append("\n");
+			dbProp.append("DriverType=org.gjt.mm.mysql.Driver");
+			dbProp.append("\n");
+			dbProp.append("databaseSchema=core");
+			dbProp.append("\n");
+			dbProp.append("databaseUsername=" + dbUser);
+			dbProp.append("\n");
+			dbProp.append("databasePassword=" + dbPass);
+			dbProp.append("\n");
 
-		StringBuffer dbProp = new StringBuffer();
-		dbProp.append("databaseConnectionURL=jdbc:mysql://" + dbHost + ":" + dbPort + "/");
-		dbProp.append("\n");
-		dbProp.append("DriverType=org.gjt.mm.mysql.Driver");
-		dbProp.append("\n");
-		dbProp.append("databaseSchema=core");
-		dbProp.append("\n");
-		dbProp.append("databaseUsername=" + dbUser);
-		dbProp.append("\n");
-		dbProp.append("databasePassword=" + dbPass);
-		dbProp.append("\n");
-
-		if (!auth.equals(dbAuth)) {
-			String dbAuthFailed = "Authentication failure. Please paste the content of the file " + Constants.SETUP_AUTH
-					+ " into the authentication field.";
-			ses.setAttribute("dbAuthFailed", dbAuthFailed);
-		} else {
-			Files.write(Paths.get(Constants.DBPROP), dbProp.toString().getBytes(), StandardOpenOption.CREATE);	
-			if (Database.getDatabaseConnection(null) == null) {
-				String dbConnectionFailed = "Connection to Database failed";
-				ses.setAttribute("dbConnectionFailed", dbConnectionFailed);
+			if (!auth.equals(dbAuth)) {
+				htmlOutput = bundle.getString("generic.text.setup.authentication.failed");
 			} else {
-				try {
-					if (dbOverride != null) {
-						executeSqlScript();
+				Files.write(Paths.get(Constants.DBPROP), dbProp.toString().getBytes(), StandardOpenOption.CREATE);	
+				if (Database.getDatabaseConnection(null) == null) {
+					htmlOutput = bundle.getString("generic.text.setup.connection.failed");
+				} else {
+					try {
+						if (dbOverride.equalsIgnoreCase("true")) {
+							executeSqlScript();
+							htmlOutput = bundle.getString("generic.text.setup.success") + " " + bundle.getString("generic.text.setup.success.overwrittendb");
+						} else {
+							htmlOutput = bundle.getString("generic.text.setup.success");
+						}
+						success = true;
+					} catch (InstallationException e) {
+						htmlOutput = bundle.getString("generic.text.setup.failed") + ": " +  e.getMessage();
+						FileUtils.deleteQuietly(new File(Constants.DBPROP));
 					}
-				} catch (InstallationException e) {
-					String dbSetupFailed = "Failed setting up the database: " +  e.getMessage();
-					ses.setAttribute("dbSetupFailed", dbSetupFailed);
-					FileUtils.deleteQuietly(new File(Constants.DBPROP));
+					//Clean up File as it is not needed anymore. Will Cause a new one to be generated next time too
+					removeAuthFile();
 				}
+
 			}
-
+			if(success) {
+				htmlOutput = "<h2 class=\"title\" id=\"login_title\">"+bundle.getString("generic.text.setup.response.success")+"</h2><p>"+htmlOutput+" "+bundle.getString("generic.text.setup.response.success.redirecting")+"</p>";
+			} else {
+				htmlOutput = "<h2 class=\"title\" id=\"login_title\">"+bundle.getString("generic.text.setup.response.failed")+"</h2><p>"+htmlOutput+"</p>";
+			}
+			out.write(htmlOutput);
 		}
-
-		response.sendRedirect("setup.jsp");
-
+		catch (Exception e)
+		{
+			out.write(errors.getString("error.funky"));
+			log.fatal("Unexpected database config creation error: " + e.toString());
+		}
+		out.close();
 	}
 
 	public static boolean isInstalled() {
@@ -102,6 +127,14 @@ public class Setup extends HttpServlet {
 		} catch (IOException e) {
 			log.fatal("Unable to generate auth");
 			e.printStackTrace();
+		}
+	}
+	
+	private static void removeAuthFile() {
+		if (!Files.exists(Paths.get(Constants.SETUP_AUTH), LinkOption.NOFOLLOW_LINKS)) {
+			log.info("Could not find " + Constants.SETUP_AUTH);
+		} else {
+			FileUtils.deleteQuietly(new File(Constants.SETUP_AUTH));
 		}
 	}
 
