6863d80758cfb05945da32a6c24059969cbda529
Robert Conan <rconan@axway.com>
2018-10-24 15:54:39 +0100

    #451 - Can now start/stop/restart docker container

--- src/main/java/servlets/api/DockerInterface.java
+++ src/main/java/servlets/api/DockerInterface.java
@@ -33,9 +33,9 @@ public class DockerInterface extends HttpServlet {
                 log.debug("Valid session");
                 DockerController docker = new DockerController();
                 String dockerID = request.getParameter("dockerID");
-                if ( dockerID != null){
+                if (dockerID != null) {
                     String logs = request.getParameter("logs");
-                    if (logs != null){
+                    if (logs != null) {
                         out.print(docker.getContainerLogs(dockerID));
                         return;
                     }
@@ -44,7 +44,7 @@ public class DockerInterface extends HttpServlet {
                     out.print(docker.getContainerData(dockerID));
                     //out.print(docker.getContainerData("058afa9b1566f31b8ebae22ec2d48476156be229c15eecbd3f175d57834d4c35"));
                     return;
-                }else {
+                } else {
                     log.info("Request received for all docker information from " + ses.getAttribute("userName").toString());
                     out.print(docker.getContainerData());
                     return;
@@ -53,11 +53,38 @@ public class DockerInterface extends HttpServlet {
         }
     }
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        DockerController docker = new DockerController();
-        docker.stopContainer("058afa9b1566f31b8ebae22ec2d48476156be229c15eecbd3f175d57834d4c35");
-        PrintWriter out = resp.getWriter();
-        out.write("Stopping Container");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Setting IpAddress To Log and taking header for original IP if forwarded from proxy
+        ShepherdLogManager.setRequestIp(request.getRemoteAddr(), request.getHeader("X-Forwarded-For"));
+        log.debug("/api/docker POST called");
+        PrintWriter out = response.getWriter();
+        HttpSession ses = request.getSession(true);
+        if (Validate.validateSession(ses)) {
+            ShepherdLogManager.setRequestIp(request.getRemoteAddr(), request.getHeader("X-Forwarded-For"), ses.getAttribute("userName").toString());
+            log.debug("Docker interface called by " + ses.getAttribute("userName").toString());
+            Cookie tokenCookie = Validate.getToken(request.getCookies());
+            Object tokenParmeter = request.getParameter("csrfToken");
+            if (Validate.validateTokens(tokenCookie, tokenParmeter)) {
+                log.debug("Valid session");
+                DockerController docker = new DockerController();
+                String dockerID = request.getParameter("dockerID");
+                if (dockerID != null) {
+                    response.setHeader("Content-Type", "application/json");
+                    log.info("POST request for docker information received for " + dockerID + " by " + ses.getAttribute("userName").toString());
+                    String action = request.getParameter("action");
+                    if (action.equalsIgnoreCase("start")) {
+                        docker.startContainer(dockerID);
+                        out.print(docker.getContainerData(dockerID));
+                    } else if (action.equalsIgnoreCase("stop")) {
+                        docker.stopContainer(dockerID);
+                        out.print(docker.getContainerData(dockerID));
+                    } else if (action.equalsIgnoreCase("restart")) {
+                        docker.restartContainer(dockerID);
+                        out.print(docker.getContainerData(dockerID));
+                    }
+                }
+            }
+        }
     }
 
 
