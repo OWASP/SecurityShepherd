152e0b3fd48b6903239d487f907b28510e1014c2
Alex Haynes <alexhaynes93@gmail.com>
2019-01-04 03:16:35 -0600

    Modify convert-sql-scripts.sh to perform sed operations correctly on … (#462)
    * Modify convert-sql-scripts.sh to perform sed operations correctly on Mac OS X
    
    * Modify gitignore to prevent committing schema files
    
    * Use emptystring for backup file
    
    * Stop breaking linux while providing a backup argument for macs

--- docker/scripts/convert-sql-scripts.sh
+++ docker/scripts/convert-sql-scripts.sh
@@ -3,11 +3,11 @@
 core="docker/mysql/coreSchema.sql"
 module="docker/mysql/moduleSchemas.sql"
 
-sed -i 's/-- DELIMITER \$\$/DELIMITER \$\$/g' ${core}
-sed -i 's/-- DELIMITER \$\$/DELIMITER \$\$/g' ${module}
+sed -i'' -e 's/-- DELIMITER \$\$/DELIMITER \$\$/g' ${core}
+sed -i'' -e 's/-- DELIMITER \$\$/DELIMITER \$\$/g' ${module}
 
-sed -i 's/-- \$\$/\$\$/g' ${core}
-sed -i 's/-- \$\$/\$\$/g' ${module}
+sed -i'' -e 's/-- \$\$/\$\$/g' ${core}
+sed -i'' -e 's/-- \$\$/\$\$/g' ${module}
 
-sed -i 's/-- DELIMITER \;/DELIMITER \;/g' ${core}
-sed -i 's/-- DELIMITER \;/DELIMITER \;/g' ${module}
+sed -i'' -e 's/-- DELIMITER \;/DELIMITER \;/g' ${core}
+sed -i'' -e 's/-- DELIMITER \;/DELIMITER \;/g' ${module}
