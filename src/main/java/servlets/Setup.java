3a1508339fe203520595ac67f073bdcb4b68d894
ismisepaul <ismisepaul@gmail.com>
2019-07-08 17:03:49 +0100

    Cleaning up

--- src/main/java/servlets/Setup.java
+++ src/main/java/servlets/Setup.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.mongodb.*;
+import com.mongodb.MongoClient;
 import dbProcs.FileInputProperties;
 import dbProcs.MongoDatabase;
 import org.apache.commons.io.FileUtils;
--- src/main/java/servlets/module/challenge/NoSqlInjection1.java
+++ src/main/java/servlets/module/challenge/NoSqlInjection1.java
@@ -11,7 +11,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.mongodb.*;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoException;
+import com.mongodb.MongoSocketException;
+import com.mongodb.MongoTimeoutException;
+
 import dbProcs.MongoDatabase;
 import org.apache.log4j.Logger;
 import org.owasp.encoder.Encode;
