111a412a73d62f14529a8b6821020350cfae267e
ismisepaul <ismisepaul@gmail.com>
2019-07-15 17:25:55 +0100

    Adding integration test for NoSQL that utilises docker from maven

--- src/test/java/servlets/module/challenge/NoSqlInjection1IT.java
+++ src/test/java/servlets/module/challenge/NoSqlInjection1IT.java
@@ -1,17 +1,8 @@
 package servlets.module.challenge;
 
-import com.github.fakemongo.Fongo;
-
 import org.apache.log4j.Logger;
-import com.mongodb.FongoDB;
-import com.mongodb.MongoClient;
-import com.mongodb.DBCollection;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
 import dbProcs.GetterTest;
 import dbProcs.Setter;
-import org.json.JSONObject;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -24,26 +15,20 @@ import testUtils.TestProperties;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
-import java.io.StringWriter;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 import static org.junit.Assert.fail;
-import static org.junit.jupiter.api.Assertions.*;
 
 
-public class NoSqlInjection1Test extends Mockito {
+public class NoSqlInjection1IT extends Mockito {
 
-    private static String MONGO_COLL_NAME = "shepherdGames";
-    private static Fongo fongo = new Fongo(MONGO_COLL_NAME);
-    private static MongoClient mongoClient;
-    private static FongoDB fakeDB;
     private static String applicationRoot = new String();
     private static String USERNAME = "lessonTester";
-    private static String TEST_DB = "shepherdTest";
     private static String LANG = "en_GB";
+    private static String NOSQL_ATTACK = "';return(true);var a='a";
 
-    private static org.apache.log4j.Logger log = Logger.getLogger(NoSqlInjection1Test.class);
+    private static org.apache.log4j.Logger log = Logger.getLogger(NoSqlInjection1IT.class);
     private ResourceBundle bundle = ResourceBundle.getBundle
             ("i18n.servlets.challenges.injection.nosql", new Locale(LANG));
 
@@ -56,8 +41,6 @@ public class NoSqlInjection1Test extends Mockito {
     @BeforeClass
     public static void initAll()
     {
-        fakeDB = fongo.getDB(TEST_DB);
-        mongoClient = fongo.getMongo();
         TestProperties.setTestPropertiesFileDirectory(log);
     }
 
@@ -70,19 +53,6 @@ public class NoSqlInjection1Test extends Mockito {
         //Open All modules
         if(!Setter.openAllModules(applicationRoot))
             fail("Could not Mark All Modules As Open");
-
-        //Populate the DB
-        DBCollection dbCollection = fakeDB.getCollection(MONGO_COLL_NAME);
-        BasicDBObject user = new BasicDBObject("_id", "c09f32d4c3dd5b75f04108e5ffc9226cd8840288a62bdaf9dc65828ab6eaf86a")
-                .append("name", "Marlo")
-                .append("address", "Baltimore");
-        dbCollection.insert(user);
-    }
-
-    @AfterClass
-    public static void teardown()
-    {
-        fakeDB.getCollection(MONGO_COLL_NAME).drop();
     }
 
 
@@ -121,29 +91,10 @@ public class NoSqlInjection1Test extends Mockito {
     }
 
 
-    @Test()
-    //@DisplayName("Normal HTTP Request to MongoDb")
-    public void testLevelSendsAnswerAndTriesToQueryMongoDb_ShouldReturn302WithMongoDbError()
+    @Test
+    public void testResultMatches()
     {
-
-        /*StringWriter stringWriter = new StringWriter();
-        PrintWriter writer = new PrintWriter(stringWriter);
-        Cursor cursor;
-        Object id;
-        Object name;
-        Object address;
-        */
-
         try {
-
-            DBCollection dbCollection = fakeDB.getCollection(MONGO_COLL_NAME);
-            DBObject firstMatched = dbCollection.findOne();
-
-            JSONObject jsonObject = new JSONObject(firstMatched.toString());
-            String solution = jsonObject.getString("_id");
-
-            log.debug("THE SOLUTION: " + solution);
-
             GetterTest.verifyTestUser(applicationRoot, USERNAME, USERNAME);
             log.debug("Signing in as " + USERNAME + " Through LoginServlet");
             TestProperties.loginDoPost(log, request, response, USERNAME, USERNAME, null, LANG);
@@ -151,21 +102,33 @@ public class NoSqlInjection1Test extends Mockito {
             if (response.getCookie("token") == null)
                 fail("No CSRF Token Was Returned from Login Servlet");
             String csrfToken = response.getCookie("token").getValue();
-            if (csrfToken.isEmpty()) {
+            if (csrfToken.isEmpty())
+            {
                 String message = new String("No CSRF token returned from Login Servlet");
                 log.fatal(message);
                 fail(message);
-            } else {
+            }
+            else
+            {
                 request.setCookies(response.getCookies());
-                String servletResponse = moduleDoPost(solution, csrfToken, 302);
-
-                assertTrue(servletResponse.contains
-                        ("<h2 class='title'>Gamer Info</h2><table><tr><th>GamerId</th><th>Name</th><th>Address</th><p>"));
+                String servletResponse = moduleDoPost(NOSQL_ATTACK, csrfToken, 302);
+                if(servletResponse.contains("An error was detected"))
+                {
+                    String message = new String("Valid Key Returned Funky Error");
+                    log.fatal(message);
+                    fail(message);
+                }
+                else if(!servletResponse.contains("Jimmy"))
+                {
+                    String message = new String("Valid Solution did not return Result Key");
+                    log.fatal(message);
+                    fail(message);
+                }
             }
         }
         catch (IOException e) {fail("IO Exception should not have been thrown");}
         catch (ServletException e) {fail("ServletException should not have been thrown");}
-        catch (Exception e) {}
+        catch (Exception e) {fail("Exception Thrown" + e);}
 
     }
 
