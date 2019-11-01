3cfd57047d5f8b3dcd7d10790b27e78a154e610f
Jonathan Jogenfors <etnoy@broach.se>
2019-11-01 21:55:40 +0100

    More cleanup of IT exception handling

--- src/it/java/servlets/admin/config/SetOpenFloorModeIT.java
+++ src/it/java/servlets/admin/config/SetOpenFloorModeIT.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.servlet.ServletException;
+
 import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -17,15 +19,14 @@ import testUtils.TestProperties;
 import utils.InstallationException;
 import utils.ModulePlan;
 
-public class SetOpenFloorModeIT
-{
+public class SetOpenFloorModeIT {
 	private static org.apache.log4j.Logger log = Logger.getLogger(SetOpenFloorModeIT.class);
 	private static String applicationRoot = new String();
 	private static String lang = "en_GB";
 	private MockHttpServletRequest request;
-    private MockHttpServletResponse response;
+	private MockHttpServletResponse response;
 
-    /**
+	/**
 	 * Creates DB or Restores DB to Factory Defaults before running tests
 	 */
 	@BeforeClass
@@ -37,112 +38,98 @@ public class SetOpenFloorModeIT
 		TestProperties.executeSql(log);
 
 	}
-    
+
 	@Before
-	public void setUp()
-	{
+	public void setUp() {
 		request = new MockHttpServletRequest();
-        response = new MockHttpServletResponse();
-        //Set Module Plan to CTF Floor
-        ModulePlan.setIncrementalFloor();
-        if(!ModulePlan.isIncrementalFloor())
-        {
-        	String message = "Unable to Set floor plan to CTF";
-        	log.fatal(message);
-        	fail(message);
-        }
+		response = new MockHttpServletResponse();
+		// Set Module Plan to CTF Floor
+		ModulePlan.setIncrementalFloor();
+		if (!ModulePlan.isIncrementalFloor()) {
+			String message = "Unable to Set floor plan to CTF";
+			log.fatal(message);
+			fail(message);
+		}
 	}
 
 	/**
 	 * Method to Simulate the interaction with the Set Open Floor Mode Servlet
-	 * @param moduleId The ID of the Module to Search For
+	 * 
+	 * @param moduleId  The ID of the Module to Search For
 	 * @param csrfToken The CSRF Token of the User
-	 * @return The Content of the Response (Which is supposed to be the location of the module)
+	 * @return The Content of the Response (Which is supposed to be the location of
+	 *         the module)
+	 * @throws ServletException 
+	 * @throws IOException 
 	 * @throws Exception
 	 */
-	public String doThePost(String csrfToken) throws Exception
-	{
+	public String doThePost(String csrfToken) throws ServletException, IOException {
 		String servletClassName = "SetOpenFloorMode";
-		try
-		{
-			int expectedResponseCode = 302;
 
-			log.debug("Creating " + servletClassName + " Servlet Instance");
-			SetOpenFloorMode servlet = new SetOpenFloorMode();
-			servlet.init(new MockServletConfig(servletClassName));
+		int expectedResponseCode = 302;
 
-			//Adding Correct CSRF Token (Token Submitted)
-			request.addParameter("csrfToken", csrfToken);
+		log.debug("Creating " + servletClassName + " Servlet Instance");
+		SetOpenFloorMode servlet = new SetOpenFloorMode();
+		servlet.init(new MockServletConfig(servletClassName));
 
-			log.debug("Running doPost");
-			servlet.doPost(request, response);
+		// Adding Correct CSRF Token (Token Submitted)
+		request.addParameter("csrfToken", csrfToken);
 
-			if(response.getStatus() != expectedResponseCode)
-				fail(servletClassName + " Servlet Returned " + response.getStatus() + " Code. " + expectedResponseCode + " Expected");
-			else
-			{
-				log.debug(expectedResponseCode + " Detected");
-				log.debug("Servlet Successful, returning response retrieved: " + response.getContentAsString());
-				return(response.getContentAsString());
-			}
+		log.debug("Running doPost");
+		servlet.doPost(request, response);
+
+		if (response.getStatus() != expectedResponseCode)
+			fail(servletClassName + " Servlet Returned " + response.getStatus() + " Code. " + expectedResponseCode
+					+ " Expected");
+		else {
+			log.debug(expectedResponseCode + " Detected");
+			log.debug("Servlet Successful, returning response retrieved: " + response.getContentAsString());
+			return (response.getContentAsString());
 		}
-		catch(Exception e)
-		{
-			throw e;
-		}
+
 		return null;
 	}
 
 	/**
-	 * This test checks that non admin users get access errors when Setting Open Floor Mode
+	 * This test checks that non admin users get access errors when Setting Open
+	 * Floor Mode
 	 */
 	@Test
-	public void testUserEnableOpenFloorMode()
-	{
+	public void testUserEnableOpenFloorMode() {
 		String userName = "configUserTester";
 		String password = userName;
-		//Verify / Create user in DB
-		try
-		{
+		// Verify / Create user in DB
+		try {
 			TestProperties.verifyTestUser(log, applicationRoot, userName, password);
-			//Sign in as Normal User
+			// Sign in as Normal User
 			log.debug("Signing in as User Through LoginServlet");
 			TestProperties.loginDoPost(log, request, response, userName, userName, null, lang);
 			log.debug("Login Servlet Complete, Getting CSRF Token");
-			if(response.getCookie("token") == null)
+			if (response.getCookie("token") == null)
 				fail("No CSRF Token Was Returned from Login Servlet");
 			String csrfToken = response.getCookie("token").getValue();
-			if(csrfToken.isEmpty())
-			{
+			if (csrfToken.isEmpty()) {
 				String message = new String("No CSRF token returned from Login Servlet");
 				log.fatal(message);
 				fail(message);
-			}
-			else
-			{
-				//Add Cookies from Response to outgoing request
+			} else {
+				// Add Cookies from Response to outgoing request
 				request.setCookies(response.getCookies());
 				String responseBody = doThePost(csrfToken);
-				if(responseBody.isEmpty())
-				{
+				if (responseBody.isEmpty()) {
 					log.debug("Expected Empty Result Recieved");
-					if(ModulePlan.isOpenFloor())
-					{
+					if (ModulePlan.isOpenFloor()) {
 						String message = "Floor Plan Changed to Open Floor after what should have been a failed request";
 						log.fatal(message);
 						fail(message);
 					}
-				}
-				else
-				{
+				} else {
 					String message = "Normal user did not get error when performing admin function";
 					log.fatal(message);
 					fail(message);
 				}
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			log.fatal("Could not Complete: " + e.toString());
 			fail("Exception Caught: " + e.toString());
 		}
@@ -152,116 +139,90 @@ public class SetOpenFloorModeIT
 	 * This test checks that admin users can enable open floor mode
 	 */
 	@Test
-	public void testAdminSetOpenFloorMode()
-	{
+	public void testAdminSetOpenFloorMode() {
 		String userName = "configAdminTester";
 		String password = userName;
-		//Verify / Create user in DB
-		try
-		{
+		// Verify / Create user in DB
+		try {
 			TestProperties.verifyTestAdmin(log, applicationRoot, userName, password);
-			//Sign in as Admin User
+			// Sign in as Admin User
 			log.debug("Signing in as User Through LoginServlet");
 			TestProperties.loginDoPost(log, request, response, userName, userName, null, lang);
 			log.debug("Login Servlet Complete, Getting CSRF Token");
-			if(response.getCookie("token") == null)
+			if (response.getCookie("token") == null)
 				fail("No CSRF Token Was Returned from Login Servlet");
 			String csrfToken = response.getCookie("token").getValue();
-			if(csrfToken.isEmpty())
-			{
+			if (csrfToken.isEmpty()) {
 				String message = new String("No CSRF token returned from Login Servlet");
 				log.fatal(message);
 				fail(message);
-			}
-			else
-			{
-				//Add Cookies from Response to outgoing request
+			} else {
+				// Add Cookies from Response to outgoing request
 				request.setCookies(response.getCookies());
 				String responseBody = doThePost(csrfToken);
-				if(responseBody.contains("Open Floor Plan Enabled"))
-				{
+				if (responseBody.contains("Open Floor Plan Enabled")) {
 					log.debug("OpenFloor Mode enabled");
-					if(!ModulePlan.isOpenFloor())
-					{
+					if (!ModulePlan.isOpenFloor()) {
 						String message = "Open Floor Mode was not actually Enabled";
 						log.fatal(message);
 						fail(message);
-					}
-					else if (ModulePlan.isIncrementalFloor())
-					{
+					} else if (ModulePlan.isIncrementalFloor()) {
 						String message = "ModulePlan still returning possitive for CTF Mode Check";
 						log.fatal(message);
 						fail(message);
 					}
-				}
-				else
-				{
+				} else {
 					String message = "Admin was unable to enable OpenFloor mode";
 					log.fatal(message);
 					fail(message);
 				}
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			log.fatal("Could not Complete: " + e.toString());
 			fail("Exception Caught: " + e.toString());
 		}
 	}
 
 	@Test
-	public void testCsrf()
-	{
+	public void testCsrf() {
 		String userName = "configAdminTester";
 		String password = userName;
-		//Verify / Create user in DB
-		try
-		{
+		// Verify / Create user in DB
+		try {
 			TestProperties.verifyTestAdmin(log, applicationRoot, userName, password);
-			//Sign in as Admin User
+			// Sign in as Admin User
 			log.debug("Signing in as User Through LoginServlet");
 			TestProperties.loginDoPost(log, request, response, userName, userName, null, lang);
 			log.debug("Login Servlet Complete, Getting CSRF Token");
-			if(response.getCookie("token") == null)
+			if (response.getCookie("token") == null)
 				fail("No CSRF Token Was Returned from Login Servlet");
 			String csrfToken = response.getCookie("token").getValue();
-			if(csrfToken.isEmpty())
-			{
+			if (csrfToken.isEmpty()) {
 				String message = new String("No CSRF token returned from Login Servlet");
 				log.fatal(message);
 				fail(message);
-			}
-			else
-			{
-				//Add Cookies from Response to outgoing request
+			} else {
+				// Add Cookies from Response to outgoing request
 				request.setCookies(response.getCookies());
 				String responseBody = doThePost("wrongToken");
-				if(responseBody.contains("Error Occurred"))
-				{
+				if (responseBody.contains("Error Occurred")) {
 					log.debug("CSRF Error Occurred");
-					if(ModulePlan.isOpenFloor())
-					{
+					if (ModulePlan.isOpenFloor()) {
 						String message = "OpenFloor Mode was enabled after what should have been a failed request";
 						log.fatal(message);
 						fail(message);
-					}
-					else if (!ModulePlan.isIncrementalFloor())
-					{
+					} else if (!ModulePlan.isIncrementalFloor()) {
 						String message = "ModulePlan no longer returning possitive for CTF Floor Check after what should have been a failed request";
 						log.fatal(message);
 						fail(message);
 					}
-				}
-				else
-				{
+				} else {
 					String message = "Registration Availability Update Failure";
 					log.fatal(message);
 					fail(message);
 				}
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			log.fatal("Could not Complete: " + e.toString());
 			fail("Exception Caught: " + e.toString());
 		}
--- src/it/java/servlets/admin/config/SetTournamentModeIT.java
+++ src/it/java/servlets/admin/config/SetTournamentModeIT.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.servlet.ServletException;
+
 import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -17,15 +19,14 @@ import testUtils.TestProperties;
 import utils.InstallationException;
 import utils.ModulePlan;
 
-public class SetTournamentModeIT
-{
+public class SetTournamentModeIT {
 	private static org.apache.log4j.Logger log = Logger.getLogger(SetTournamentModeIT.class);
 	private static String applicationRoot = new String();
 	private static String lang = "en_GB";
 	private MockHttpServletRequest request;
-    private MockHttpServletResponse response;
+	private MockHttpServletResponse response;
 
-    /**
+	/**
 	 * Creates DB or Restores DB to Factory Defaults before running tests
 	 */
 	@BeforeClass
@@ -37,225 +38,183 @@ public class SetTournamentModeIT
 		TestProperties.executeSql(log);
 
 	}
-    
+
 	@Before
-	public void setUp()
-	{
+	public void setUp() {
 		request = new MockHttpServletRequest();
-        response = new MockHttpServletResponse();
-        //Set Module Plan to CTF Floor
-        ModulePlan.setIncrementalFloor();
-        if(!ModulePlan.isIncrementalFloor())
-        {
-        	String message = "Unable to Set floor plan to CTF";
-        	log.fatal(message);
-        	fail(message);
-        }
+		response = new MockHttpServletResponse();
+		// Set Module Plan to CTF Floor
+		ModulePlan.setIncrementalFloor();
+		if (!ModulePlan.isIncrementalFloor()) {
+			String message = "Unable to Set floor plan to CTF";
+			log.fatal(message);
+			fail(message);
+		}
 	}
 
 	/**
 	 * Method to Simulate the interaction with the Set Tournament Mode Servlet
-	 * @param moduleId The ID of the Module to Search For
+	 * 
+	 * @param moduleId  The ID of the Module to Search For
 	 * @param csrfToken The CSRF Token of the User
-	 * @return The Content of the Response (Which is supposed to be the location of the module)
+	 * @return The Content of the Response (Which is supposed to be the location of
+	 *         the module)
+	 * @throws ServletException 
+	 * @throws IOException 
 	 * @throws Exception
 	 */
-	public String doThePost(String csrfToken) throws Exception
-	{
+	public String doThePost(String csrfToken) throws ServletException, IOException {
 		String servletClassName = "SetTournamentMode";
-		try
-		{
-			int expectedResponseCode = 302;
 
-			log.debug("Creating " + servletClassName + " Servlet Instance");
-			SetTournamentMode servlet = new SetTournamentMode();
-			servlet.init(new MockServletConfig(servletClassName));
+		int expectedResponseCode = 302;
 
-			//Adding Correct CSRF Token (Token Submitted)
-			request.addParameter("csrfToken", csrfToken);
+		log.debug("Creating " + servletClassName + " Servlet Instance");
+		SetTournamentMode servlet = new SetTournamentMode();
+		servlet.init(new MockServletConfig(servletClassName));
 
-			log.debug("Running doPost");
-			servlet.doPost(request, response);
+		// Adding Correct CSRF Token (Token Submitted)
+		request.addParameter("csrfToken", csrfToken);
 
-			if(response.getStatus() != expectedResponseCode)
-				fail(servletClassName + " Servlet Returned " + response.getStatus() + " Code. " + expectedResponseCode + " Expected");
-			else
-			{
-				log.debug(expectedResponseCode + " Detected");
-				log.debug("Servlet Successful, returning response retrieved: " + response.getContentAsString());
-				return(response.getContentAsString());
-			}
-		}
-		catch(Exception e)
-		{
-			throw e;
+		log.debug("Running doPost");
+		servlet.doPost(request, response);
+
+		if (response.getStatus() != expectedResponseCode)
+			fail(servletClassName + " Servlet Returned " + response.getStatus() + " Code. " + expectedResponseCode
+					+ " Expected");
+		else {
+			log.debug(expectedResponseCode + " Detected");
+			log.debug("Servlet Successful, returning response retrieved: " + response.getContentAsString());
+			return (response.getContentAsString());
 		}
 		return null;
 	}
 
 	@Test
-	public void testUserEnableTournamentMode()
-	{
+	public void testUserEnableTournamentMode() {
 		String userName = "configUserTester";
 		String password = userName;
-		//Verify / Create user in DB
-		try
-		{
+		// Verify / Create user in DB
+		try {
 			TestProperties.verifyTestUser(log, applicationRoot, userName, password);
-			//Sign in as Normal User
+			// Sign in as Normal User
 			log.debug("Signing in as User Through LoginServlet");
 			TestProperties.loginDoPost(log, request, response, userName, userName, null, lang);
 			log.debug("Login Servlet Complete, Getting CSRF Token");
-			if(response.getCookie("token") == null)
+			if (response.getCookie("token") == null)
 				fail("No CSRF Token Was Returned from Login Servlet");
 			String csrfToken = response.getCookie("token").getValue();
-			if(csrfToken.isEmpty())
-			{
+			if (csrfToken.isEmpty()) {
 				String message = new String("No CSRF token returned from Login Servlet");
 				log.fatal(message);
 				fail(message);
-			}
-			else
-			{
-				//Add Cookies from Response to outgoing request
+			} else {
+				// Add Cookies from Response to outgoing request
 				request.setCookies(response.getCookies());
 				String responseBody = doThePost(csrfToken);
-				if(responseBody.isEmpty())
-				{
+				if (responseBody.isEmpty()) {
 					log.debug("Expected Empty Result Recieved");
-					if(ModulePlan.isTournyFloor())
-					{
+					if (ModulePlan.isTournyFloor()) {
 						String message = "Floor Plan Changed to Tournament after what should have been a failed request";
 						log.fatal(message);
 						fail(message);
 					}
-				}
-				else
-				{
+				} else {
 					String message = "Normal user did not get error when performing admin function";
 					log.fatal(message);
 					fail(message);
 				}
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			log.fatal("Could not Complete: " + e.toString());
 			fail("Exception Caught: " + e.toString());
 		}
 	}
 
 	@Test
-	public void testAdminSetTournamentMode()
-	{
+	public void testAdminSetTournamentMode() {
 		String userName = "configAdminTester";
 		String password = userName;
-		//Verify / Create user in DB
-		try
-		{
+		// Verify / Create user in DB
+		try {
 			TestProperties.verifyTestAdmin(log, applicationRoot, userName, password);
-			//Sign in as Admin User
+			// Sign in as Admin User
 			log.debug("Signing in as User Through LoginServlet");
 			TestProperties.loginDoPost(log, request, response, userName, userName, null, lang);
 			log.debug("Login Servlet Complete, Getting CSRF Token");
-			if(response.getCookie("token") == null)
+			if (response.getCookie("token") == null)
 				fail("No CSRF Token Was Returned from Login Servlet");
 			String csrfToken = response.getCookie("token").getValue();
-			if(csrfToken.isEmpty())
-			{
+			if (csrfToken.isEmpty()) {
 				String message = new String("No CSRF token returned from Login Servlet");
 				log.fatal(message);
 				fail(message);
-			}
-			else
-			{
-				//Add Cookies from Response to outgoing request
+			} else {
+				// Add Cookies from Response to outgoing request
 				request.setCookies(response.getCookies());
 				String responseBody = doThePost(csrfToken);
-				if(responseBody.contains("Tournament Floor Plan Enabled"))
-				{
+				if (responseBody.contains("Tournament Floor Plan Enabled")) {
 					log.debug("Tournament Mode enabled");
-					if(!ModulePlan.isTournyFloor())
-					{
+					if (!ModulePlan.isTournyFloor()) {
 						String message = "Tournament Mode was not actually Enabled";
 						log.fatal(message);
 						fail(message);
-					}
-					else if (ModulePlan.isIncrementalFloor())
-					{
+					} else if (ModulePlan.isIncrementalFloor()) {
 						String message = "ModulePlan still returning possitive for CTF Mode Check";
 						log.fatal(message);
 						fail(message);
 					}
-				}
-				else
-				{
+				} else {
 					String message = "Admin was unable to enable Tournament mode";
 					log.fatal(message);
 					fail(message);
 				}
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			log.fatal("Could not Complete: " + e.toString());
 			fail("Exception Caught: " + e.toString());
 		}
 	}
 
 	@Test
-	public void testCsrf()
-	{
+	public void testCsrf() {
 		String userName = "configAdminTester";
 		String password = userName;
-		//Verify / Create user in DB
-		try
-		{
+		// Verify / Create user in DB
+		try {
 			TestProperties.verifyTestAdmin(log, applicationRoot, userName, password);
-			//Sign in as Admin User
+			// Sign in as Admin User
 			log.debug("Signing in as User Through LoginServlet");
 			TestProperties.loginDoPost(log, request, response, userName, userName, null, lang);
 			log.debug("Login Servlet Complete, Getting CSRF Token");
-			if(response.getCookie("token") == null)
+			if (response.getCookie("token") == null)
 				fail("No CSRF Token Was Returned from Login Servlet");
 			String csrfToken = response.getCookie("token").getValue();
-			if(csrfToken.isEmpty())
-			{
+			if (csrfToken.isEmpty()) {
 				String message = new String("No CSRF token returned from Login Servlet");
 				log.fatal(message);
 				fail(message);
-			}
-			else
-			{
-				//Add Cookies from Response to outgoing request
+			} else {
+				// Add Cookies from Response to outgoing request
 				request.setCookies(response.getCookies());
 				String responseBody = doThePost("wrongToken");
-				if(responseBody.contains("Error Occurred"))
-				{
+				if (responseBody.contains("Error Occurred")) {
 					log.debug("CSRF Error Occurred");
-					if(ModulePlan.isTournyFloor())
-					{
+					if (ModulePlan.isTournyFloor()) {
 						String message = "Tournament Mode was enabled after what should have been a failed request";
 						log.fatal(message);
 						fail(message);
-					}
-					else if (!ModulePlan.isIncrementalFloor())
-					{
+					} else if (!ModulePlan.isIncrementalFloor()) {
 						String message = "ModulePlan no longer returning possitive for CTF Floor Check after what should have been a failed request";
 						log.fatal(message);
 						fail(message);
 					}
-				}
-				else
-				{
+				} else {
 					String message = "No CSRF Failure Occurred";
 					log.fatal(message);
 					fail(message);
 				}
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			log.fatal("Could not Complete: " + e.toString());
 			fail("Exception Caught: " + e.toString());
 		}
