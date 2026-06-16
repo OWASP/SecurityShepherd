60545af7d5d2f025c2f2411298d4754f3d05c4e7
DuggSe01 <DuggSe01@users.noreply.github.com>
2026-06-16 10:38:26 +0100

    fix: use DbTime.UTC.get() to match ThreadLocal API in DbTime.java

--- src/main/java/dbProcs/Getter.java
+++ src/main/java/dbProcs/Getter.java
@@ -113,7 +113,7 @@ public class Getter {
           badLoginCount = userResult.getInt(5);
           tempPassword = userResult.getBoolean(6);
           classId = userResult.getString(7);
-          suspendedUntil = userResult.getTimestamp(8, DbTime.utcCalendar());
+          suspendedUntil = userResult.getTimestamp(8, DbTime.UTC.get());
           loginType = userResult.getString(9);
           tempUsername = userResult.getBoolean(10);
         } else {
@@ -238,7 +238,7 @@ public class Getter {
             // User found if a row is in the database
             userFound = true;
             log.debug("User Found");
-            suspendedUntil = userResult.getTimestamp(1, DbTime.utcCalendar());
+            suspendedUntil = userResult.getTimestamp(1, DbTime.UTC.get());
           } else {
             userFound = false;
           }
